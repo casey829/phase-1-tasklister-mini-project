1724fe8aa67ecb5434800e757c5defe0e0dd506d
casey <caseymwangi31@gmail.com>
2024-04-07 23:43:26 +0300

    learnign

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskDescriptionInput = document.getElementById("new-task-description");
+    const taskDescription = taskDescriptionInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    taskList.appendChild(taskItem);
+
+    taskDescriptionInput.value = "";
+  });
 });
